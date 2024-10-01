72b69b6e96bb18ea3b5d97ce82982533d9b4d0ab
dsv <zxcv>
2024-10-01 17:47:26 +0500

    Initial Commit

---
+++ Mess Hostel/Mess_HostelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mess_HostelApp.swift
+//  Mess Hostel
+//
+//  Created by Muhammad Rizwan on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Mess_HostelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
